9dc1b6c60eb17483e70e50eb45e8a5d999516887
Faisal Akhbar <faisalakhbar@hotmail.co.id>
2022-04-06 11:04:01 +0700

    portofolio tailwindcss

---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+module.exports = {
+  content:  [  './application/views/**/*.{php, js, html}',
+              '.assets/**/*.{php, js, html}'
+            ],
+  theme: {
+    container: {
+      center: true,
+      padding: '16px',
+    },
+    extend: {
+      colors: {
+        primary: '#14b8a6',
+        dark: '#0f172a',
+        secondary: '#64748b',
+      },
+      screens: {
+        '2xl': '1320px',
+      }
+    },
+  },
+  plugins: [],
+}
